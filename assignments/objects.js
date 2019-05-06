64f6a3525b836ddaa43850d3f21e97337952a94c
Michael Harris <mhguitar1997@gmail.com>
2019-05-06 13:06:29 -0700

    Attempted stretch goals (poorly)

--- assignments/objects.js
+++ assignments/objects.js
@@ -54,35 +54,35 @@ const antonietta = {
 // ==== Challenge 2: Reading Object Data ==== 
 // Once your objects are created, log out the following requests from HR into the console:
 
-//Mitzi's name
-console.log(mitzi.name);
+// //Mitzi's name
+// console.log(mitzi.name);
 
-// Kennan's ID
-console.log(kennan.id);
+// // Kennan's ID
+// console.log(kennan.id);
 
-// Keven's email
-console.log(keven["email"]);
+// // Keven's email
+// console.log(keven["email"]);
 
-// Gannie's name
-console.log(gannie["name"]);
+// // Gannie's name
+// console.log(gannie["name"]);
 
-// Antonietta's Gender
-console.log(antonietta.gender);
+// // Antonietta's Gender
+// console.log(antonietta.gender);
 
-// ==== Challenge 3: Object Methods ==== 
-// Give Kennan the ability to say "Hello, my name is Kennan!" Use the console.log provided as a hint.
+// // ==== Challenge 3: Object Methods ==== 
+// // Give Kennan the ability to say "Hello, my name is Kennan!" Use the console.log provided as a hint.
 
-kennan.speak = function(){
-  return `Hello, my name is ${this.name}`;
-  // return "Hello, my name is Kennan!;
-}
-console.log(kennan.speak());
+// kennan.speak = function(){
+//   return `Hello, my name is ${this.name}`;
+//   // return "Hello, my name is Kennan!;
+// }
+// console.log(kennan.speak());
 
-// Antonietta loves math, give her the ability to multiply two numbers together and return the product. Use the console.log provided as a hint.
-antonietta.multiplyNums = function(num1, num2){
-  return num1 * num2;
-}
-console.log(antonietta.multiplyNums(3,4));
+// // Antonietta loves math, give her the ability to multiply two numbers together and return the product. Use the console.log provided as a hint.
+// antonietta.multiplyNums = function(num1, num2){
+//   return num1 * num2;
+// }
+// console.log(antonietta.multiplyNums(3,4));
 
 // === Great work! === Head over to the the arrays.js file or take a look at the stretch challenge
 
@@ -93,16 +93,42 @@ console.log(antonietta.multiplyNums(3,4));
 // 3. Nest a grandchild object in the child object with properties for name and age.  The name will be Sam and the age will be 30
 // 4. Give each of the objects the ability to speak their names using the this keyword.
 
-const parent = {}
+const parent = {
+  name: "Susan",
+  age: 70,
+  child: {
+    name: "George",
+    age: 50,
+    grandchild: {
+      name: "Sam",
+      age: 30
+    }
+  }
+}
 
 // Log the parent object's name
+console.log(parent.name);
 
 // Log the child's age
+console.log(parent.child.age);
 
 // Log the name and age of the grandchild
+console.log(parent.child.grandchild.name);
 
 // Have the parent speak
+parent.speak = function() {
+  return `Hey, My name is ${this.name}!`;
+}
+console.log(parent.speak());
 
 // Have the child speak
+parent.child.speak = function(){
+  return `Hi, My name is ${this.name}`;
+}
+console.log(parent.child.speak());
 
 // Have the grandchild speak
+parent.child.grandchild.speak = function(){
+  return `Hi, My name is ${this.name}`;
+}
+console.log(parent.child.grandchild.speak());
